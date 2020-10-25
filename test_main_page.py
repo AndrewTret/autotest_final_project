b4f90cd09763610445e84a44da1b4dd794a24dc1
Andrew Tretyakov <andtrety@gmail.com>
2020-10-25 16:57:43 +0300

    add new login_link&page tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,10 @@
-from .pages.main_page import MainPage
+from .pages.main_page import BasePage
 from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)
+    main_page = BasePage(browser, link)
     main_page.open()
     main_page.go_to_login_page()
     main_page.should_be_login_link()
